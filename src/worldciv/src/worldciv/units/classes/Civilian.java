bf84ba8eda1044dde14953c341272c9eb48d5088
Anonymous Agent <121668381+AnonymousAgent789@users.noreply.github.com>
2024-09-08 17:15:39 +0100

    fixing bugs

--- src/worldciv/src/worldciv/units/classes/Civilian.java
+++
@@ -1,8 +0,0 @@
-package worldciv.units.classes;
-
-public interface Civilian {
-	
-	public void completeAction(String action);
-	public void captured(String owner);
-	
-}
--- src/worldciv/units/classes/Military.java
+++
@@ -1,8 +0,0 @@
-package worldciv.units.classes;
-
-public interface Military {
-
-	public void attack(int opponentStrength);
-	public void defend(int opponentStrength);
-	
-}
