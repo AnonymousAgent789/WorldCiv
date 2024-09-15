9bb20715c28210dbf7057976da2add31550fdbca
Abdulaleem Hanoun <ahanoun1@outlook.com>
2024-09-15 17:25:44 +0100

    Updated version

--- src/worldciv/main/WorldCiv.java
+++ src/worldciv/main/WorldCiv.java
@@ -2,8 +2,8 @@ package worldciv.main;
 
 public class WorldCiv {
 	
-	public static final String GAME_VERSION = "0.3.5";
-	public static final int BUILD_NUMBER = 8;
+	public static final String GAME_VERSION = "0.3.6";
+	public static final int BUILD_NUMBER = 9;
 	
 	public static void main(String[] args) {
 		
